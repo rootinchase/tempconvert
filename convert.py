6f1d00f9c564687452c394f1e7f9d9dbe6848cb3
Ryan Chase <rootinchase@gmail.com>
2017-10-06 09:42:01 -0700

    renaming varibles
    renamed the conversion function varibles, added (buggy, unadvertised) celsius and Fahrenheit to Kelvin

--- convert.py
+++ convert.py
@@ -3,37 +3,58 @@
 # by: Ryan Chase
 
 def main():
-    print("This program to convert Celsius tempratures to Fahrenheit")
+    print("This program to convert tempratures betweeen units")
     for x in range (5):
         #this gets the user specified temprature
         temp_first = eval(input("What is the first temperature? "))
         #we now need to know what conversion they want
-        print("What unit of temprature is this?")
-        unit = input('Enter "C" for Celsius, or "F" for Fahrenheit')
-        if unit == "C":
-                    temp_second = convert_celsius(temp_first)
-                    print("The temperature is", temp_second, "degrees Fahrenheit.")
+        print("What unit of temprature is this in?")
+        unit_current = input('Enter "C" for Celsius, or "F" for Fahrenheit: ')
+        #This is to select the final unitfinal unit selection
+        print("What do you want this in? ")
+        unit_final = input('Enter "C" for Celsius, or "F" for Fahrenheit: ')
+        if (unit_current == "C" or unit_current == "c") and (unit_final == "F" or unit_final == "f"):
+            #This is how it converts Celsius to Fahrenheit
+            temp_second = convert_celsius(temp_first)
+            print("The temperature is", temp_second, "degrees Fahrenheit.")
                     
-        elif unit == "F":
-            #This is how it calls upon the conversion function for Fahrenheit
+        elif (unit_current == "F" or unit_current == "f") and (unit_final == "C" or unit_final == "c"):
+            Celsius
             temp_second = convert_fahrenheit(temp_first)
             #This outputs the converted user specified temprature
             print("The temperature is", temp_second, "degrees Celsius.")
-                    
+
+        elif (unit_current == "F" or unit_current == "f") and (unit_final == "k" or unit_final == "K"):
+            #This is how it converts Fahrenheit to Kelvin
+            #First It converts Fahrenheit to Celsius
+            temp_second = convert_fahrenheit(temp_first)
+            #Then in converts Celsius to Kelvin
+            temp_final = convert_kelvin(temp_second)
+            print("The temperature is", temp_final, "degrees Kelvin")
+
+        elif (unit_current == "C" or unit_current == "c") and (unit_final == "k" or unit_final == "K"):
+            #This is how it converts Celsuis to Kelvin
+            temp_second = convert_kelvin(temp_first)
+            print("The temperature is", temp_second, "degrees Kelvin")
+                   
         else:
             print("Unsupported input, please try again.")
             x = x-1
 
-def convert_celsius(temp_first):
+def convert_celsius(temp_input):
     #This function converts celsius to fahrenheit
-    temp_second = 9/5 * temp_first + 32
-    return temp_second
+    temp_output = 9/5 * temp_input + 32
+    return temp_output
     
-def convert_fahrenheit(temp_first):
+def convert_fahrenheit(temp_input):
     #This function converts fahrenheit to celcius (not used yet)
-    temp_second = (temp_first - 32) * 5/9
-    return temp_second
-    
+    temp_output = (temp_input - 32) * 5/9
+    return temp_output
+
+def convert_kelvin(temp_input):
+    #This function converts celsius to kelvin
+    temp_output = temp - 273.15
+    return temp_output
 
 def table():
     #this creates a table of celcius - fahrenheit conversions
